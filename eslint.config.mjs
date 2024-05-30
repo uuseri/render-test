6c3d19e3b9880f9c01150a41c3f50e3bc657035e
uuseri <ehdotonmahdollisesti@proton.me>
2024-05-30 14:42:24 +0300

    part4 Sovelluksen rakenne

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,7 +18,7 @@ export default [
       '@stylistic/js/indent': ['error', 2],
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
-      '@stylistic/js/semi': ['error', 'always'],
+      '@stylistic/js/semi': ['off'],
       '@stylistic/js/no-trailing-spaces': ['error', { 'skipBlankLines': true, 'ignoreComments': true }],
       '@stylistic/js/object-curly-spacing': ['error', 'always'],
       '@stylistic/js/arrow-spacing': ['error', { 'before': true, 'after': true }]
--- models/note.js
+++ models/note.js
@@ -1,35 +1,20 @@
-/*eslint no-undef: 'off'*/
-/*eslint no-unused-vars: 'off'*/
-const mongoose = require('mongoose');
-
-mongoose.set('strictQuery', false);
-
-const url = process.env.MONGODB_URI;
-
-console.log('connecting to', url);
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minlength: 5,
-    required: true
+    require: true,
+    minlength: 5
   },
   important: Boolean,
-});
+})
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject._v
   }
-});
+})
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
